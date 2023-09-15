e7404f05da3c6de65def101a43a9ab3e145c34e5
covan <titanslegend2000@gmail.com>
2023-09-16 00:35:20 +0700

    create service and data persistence layer for Book domain object

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookRepository.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice.domain;
+
+public interface BookRepository {
+    Iterable<Book> findAll();
+
+    Optional<Book> findByIsbn(String isbn);
+
+    boolean existsByIsbn(String isbn);
+
+    Book save(Book book);
+
+    void deleteByIsbn(String isbn);
+}
